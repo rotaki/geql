fe82ed723ea14e520b51ace5eda64dd6f1866f84
Fredrik Nilsson <fredrik.pj.nilsson@gmail.com>
2019-05-09 01:38:27 +0200

    Improved trajectory code

--- impl/Trajectory.py
+++ impl/Trajectory.py
@@ -4,21 +4,29 @@ from RLInterfaces import IQEstimator
 import numpy as np
 
 Transition = namedtuple('Transition', [
-    'state',    # state
-    'action',   # action taken in state
-    'terminal', # True iff action resulted in terminal state
-    'reward',   # reward associated with taking A in S
-    'q'         # temporal Q(s, a) estimated when backing up the trajectory
+    'action',    # action taken in previous state
+    'reward',    # reward associated with taking A in S
+    'state',     # resulting state
+    'terminal',  # True iff action resulted in terminal state
+    'lp_action'  # action selected in state by learning policy (or None)
+ ])
+
+QSample = namedtuple('QSample', [
+    'state',
+    'action',
+    'q',
+    'importance'
 ])
 
 class Trajectory:
-    def __init__(self):
-        self.transitions = []
+    def __init__(self, initial_state, is_terminal=False):
+        self.transitions = [Transition(None, None, initial_state, is_terminal, None)]
         
-    def add_transition(self, state, action, terminal, reward):
-        self.transitions.append(Transition(state, action, terminal, reward, None))
+    def add_transition(self, action, reward, state, terminal, lp_action):
+        self.transitions.append(Transition(action, reward, state, terminal, lp_action))
 
     def sarsa_backup(self, entry, discount, steps, q_estimator):
+        entry += 1
         discounted_reward = 0
         reached_terminal_state = False
         for forward_step in range(steps):
@@ -32,35 +40,36 @@ class Trajectory:
             
         if not reached_terminal_state:
             # Include the Q(s, a) of the last step on the horizon
-            horizon_entry = self.transitions[entry + steps]
+            horizon_entry = self.transitions[entry + steps - 1]
             discounted_reward += pow(discount, steps) * \
-                q_estimator.estimate(horizon_entry.state, horizon_entry.action)
-        return discounted_reward
+                q_estimator.estimate(horizon_entry.state, horizon_entry.lp_action)
+
+        return QSample(self.transitions[entry - 1].state,
+                       self.transitions[entry].action,
+                       discounted_reward,
+                       1.0) # Importance always 1.0 for SARSA
             
     def sarsa_episode_backup(self, discount, steps, q_estimator):
-        return [self.transitions[i]._replace(
-            q=self.sarsa_backup(i, discount, steps, q_estimator))
-            for i in range(len(self.transitions))]
+        return [self.sarsa_backup(i, discount, steps, q_estimator)
+                for i in range(len(self.transitions) - 1)]
 
     def q_backup(self, entry, discount, steps, q_estimator, q_action_policy):
         if steps != 1:
             # Check Sutton chapter 7.3 and 7.5 for possible implementation
             raise NotImplementedError('Not yet implemented')
+        entry += 1
         discounted_reward = self.transitions[entry].reward
         if not self.transitions[entry].terminal:
-            if entry + 1 >= len(self.transitions):
-                raise RuntimeError('Trajectory does not contain the next state yet')
-            next_state = self.transitions[entry + 1].state
-            q_action = q_action_policy.get_action(next_state, q_estimator)
-            discounted_reward += q_estimator.estimate(next_state, q_action)
-        self.transitions[entry] = self.transitions[entry]._replace(q=discounted_reward)
+            q_action = q_action_policy.get_action(self.transitions[entry].state, q_estimator)
+            discounted_reward += discount * q_estimator.estimate(self.transitions[entry].state, q_action)
+        return QSample(self.transitions[entry - 1].state,
+                       self.transitions[entry].action,
+                       discounted_reward,
+                       1.0) # If we implement n-step off policy, importance factor will need to change
     
     def q_episode_backup(self, discount, steps, q_estimator, q_action_policy):
-        return [self.transitions[i]._replace(
-            q=self.sarsa_backup(i, discount, steps, q_estimator))
-            for i in range(len(self.transitions))]
-    
-
+        return [self.q_backup(i, discount, steps, q_estimator, q_action_policy)
+                for i in range(len(self.transitions) - 1)]
             
 ### Unit tests below
 import unittest
@@ -83,7 +92,7 @@ class TestBackupTrajectory(unittest.TestCase):
     def setUp(self):
         states = [np.random.rand(240, 256, 3) for _ in range(10)]
         self.trajectory_data = [
-            # S         A   R   S'         A'
+            # S0        A1  R2  S'3        A'4
             (states[0], 0,  3,  states[1], 1),
             (states[1], 1,  2,  states[2], 2),
             (states[2], 2, -1,  states[3], 0),
@@ -119,9 +128,9 @@ class TestBackupTrajectory(unittest.TestCase):
 
 
     def test_sarsa_1step(self):
-        trajectory = Trajectory()
+        trajectory = Trajectory(self.trajectory_data[0][0])
         for t in self.trajectory_data:
-            trajectory.add_transition(t[0], t[1], t[4] == None, t[2])
+            trajectory.add_transition(t[1], t[2], t[3], t[4] == None, t[4])
         tl = trajectory.sarsa_episode_backup(0.8, 1, self.estimator)
 
         # Manually calculated values
@@ -131,11 +140,18 @@ class TestBackupTrajectory(unittest.TestCase):
         self.assertAlmostEqual(tl[3].q, 23.8)
         self.assertAlmostEqual(tl[4].q, 52.2)
         self.assertAlmostEqual(tl[5].q, 50.0)
+
+        # Check we're getting the correct array as "from"-state
+        self.assertTrue(np.array_equal(tl[1].state, self.trajectory_data[0][3]))
+        # ...and correct action
+        self.assertEqual(tl[1].action, 1)
+        self.assertEqual(tl[1].importance, 1.0)
         
     def test_sarsa_3step(self):
-        trajectory = Trajectory()
+        trajectory = Trajectory(self.trajectory_data[0][0])
         for t in self.trajectory_data:
-            trajectory.add_transition(t[0], t[1], t[4] == None, t[2])
+            trajectory.add_transition(t[1], t[2], t[3], t[4] == None, t[4])
+
         tl = trajectory.sarsa_episode_backup(0.8, 3, self.estimator)
 
         # Manually calculated values
@@ -148,11 +164,18 @@ class TestBackupTrajectory(unittest.TestCase):
 
 
     def test_q_1step(self):
-        trajectory = Trajectory()
-        greedy = EpsilonGreedyActionPolicy([0, 1, 2], 0.0)
+        trajectory = Trajectory(self.trajectory_data[0][0])
         for t in self.trajectory_data:
-            trajectory.add_transition(t[0], t[1], t[4] == None, t[2])
-        trajectory.q_episode_backup(0.8, 1, self.estimator, greedy)
+            trajectory.add_transition(t[1], t[2], t[3], t[4] == None, t[4])
+        greedy = EpsilonGreedyActionPolicy([0, 1, 2], 0.0)
+        
+        tl = trajectory.q_episode_backup(0.8, 1, self.estimator, greedy)
+        self.assertAlmostEqual(tl[0].q, 13.4)
+        self.assertAlmostEqual(tl[1].q, 20.4)
+        self.assertAlmostEqual(tl[2].q, 28.6)
+        self.assertAlmostEqual(tl[3].q, 28.6)
+        self.assertAlmostEqual(tl[4].q, 53.8)
+        self.assertAlmostEqual(tl[5].q, 50)
     
 if __name__ == '__main__':
     unittest.main()
