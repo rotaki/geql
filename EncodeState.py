05621dd381e549f2a0311daa87466c7ef13a458c
richard <wattlebirdaz@gmail.com>
2019-05-13 15:26:26 +0200

    digitize state

--- EncodeState.py
+++ EncodeState.py
@@ -5,7 +5,10 @@ import numpy as np
 """
 * encode state for training + predicting(States are optimized for different learning methods)
 """
-class EncodeState:        
+class EncodeState:
+    def digitize_state(self, state_encoding_params):
+        return np.linspace(0, 256, state_encoding_params.pixel_intensity + 1)[:-1]
+    
     def encode_state(self, clustering_method, state, state_encoding_params):
         if (clustering_method == "kmeans"):
             img = Image.fromarray(state)
@@ -16,7 +19,7 @@ class EncodeState:
             new_width = int(state_encoding_params.default_shape[0]/state_encoding_params.resize_factor)
             new_height = int(state_encoding_params.default_shape[1]/state_encoding_params.resize_factor)
             resized_img = img.resize((new_width, new_height), resample=Image.BICUBIC)
-            pi = state_encoding_params.pixel_intensity
-            imgtoArray = np.ceil(np.asarray(resized_img).reshape(-1)/pi)*pi + 256/(2*pi)
+            imgtoArray = np.asarray(resized_img).reshape(-1)
+            imgtoArray = np.digitize(imgtoArray, bins=self.digitize_state(state_encoding_params))*(256/state_encoding_params.pixel_intensity)
             return imgtoArray
 
