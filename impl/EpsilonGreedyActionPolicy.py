49886d3efde0fd65cc51621b0d2a9803cff22e1a
Fredrik Nilsson <fredrik.pj.nilsson@gmail.com>
2019-05-14 02:40:47 +0200

    Added epsilon decay to e-greedy

--- impl/EpsilonGreedyActionPolicy.py
+++ impl/EpsilonGreedyActionPolicy.py
@@ -2,12 +2,16 @@ from RLInterfaces import IActionPolicy
 import random
 
 class EpsilonGreedyActionPolicy (IActionPolicy):
-    def __init__(self, actions, epsilon):
+    def __init__(self, actions, epsilon, decay_factor = 1, decay_interval = 10000):
         self.epsilon = epsilon
         self.actions = actions
+        self.decay_factor = decay_factor
+        self.decay_interval = decay_interval
+        self.episodes_seen = 0
+        self.current_epsilon = epsilon
 
     def get_action(self, state, q_estimator):
-        if random.random() < self.epsilon:
+        if random.random() < self.current_epsilon:
             # Choose randomly
             return random.choice(self.actions)
         else:
@@ -18,7 +22,17 @@ class EpsilonGreedyActionPolicy (IActionPolicy):
             chosen = random.choice(candidates)
             return chosen[0]
 
+    def episode_finished(self):
+        self.episodes_seen += 1
+        self.current_epsilon = self.epsilon * \
+                               pow(self.decay_factor, self.episodes_seen / self.decay_interval)
+        print('Current epsilon: {}'.format(self.current_epsilon))
+                
     def summary(self):
-        return '$\epsilon-greedy$ [$\epsilon = {}$, $|A| = {}$]'.format(self.epsilon, len(self.actions))
+        return '$\epsilon-greedy$ [$\epsilon = {}$, decay = ${}^\\frac{{e}}{{{}}}$, $|A| = {}$]'.format(
+            self.epsilon,
+            self.decay_factor,
+            self.decay_interval,
+            len(self.actions))
 
     # TODO: Load/save
