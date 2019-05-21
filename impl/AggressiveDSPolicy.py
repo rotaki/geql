303473274a1a495c794553aaa92ced16de5da4c1
Ubuntu <ubuntu@ip-172-31-38-166.eu-north-1.compute.internal>
2019-05-21 21:15:49 +0000

    decay epsilon

--- impl/AggressiveDSPolicy.py
+++ impl/AggressiveDSPolicy.py
@@ -4,12 +4,16 @@ import numpy as np
 import random
 
 class AggressiveDSPolicy(IActionPolicy):
-    def __init__(self, actions, epsilon, state_encoding_params):
+    def __init__(self, actions, epsilon, state_encoding_params, decay_factor=1, decay_interval=10000):
         self.epsilon = epsilon
         self.actions = actions
         self.s_e_p = state_encoding_params
         self.action_counter = dict()
-
+        self.decay_factor = decay_factor
+        self.decay_interval = decay_interval
+        self.episodes_seen = 0
+        self.current_epsilon = epsilon
+        
     def show_action_count(self):
         return self.action_counter
 
@@ -30,7 +34,7 @@ class AggressiveDSPolicy(IActionPolicy):
         encoded_state = self.encode_state(state)
         if encoded_state not in self.action_counter:
             self.action_counter[encoded_state] = [0 for i in range(len(self.actions))]
-        if random.random() < self.epsilon:
+        if random.random() < self.current_epsilon:
             mask = self.gibbs_action_count(encoded_state)
             try:
                 action_choice = np.random.choice(self.actions, p=mask)
@@ -46,6 +50,11 @@ class AggressiveDSPolicy(IActionPolicy):
             chosen = random.choice(candidates)
             self.add_action_count(encoded_state, chosen[0])
             return chosen[0]
+
+    def episode_finished(self):
+        self.episodes_seen += 1
+        self.current_epsilon = self.epsilon * pow(self.decay_factor, self.episodes_seen / self.decay_interval)
+        print('Current epsilon: {}'.format(self.current_epsilon))
 
     def summary(self):
         return 'Agressive DS $\epsilon-greedy$ [$\epsilon = {}$, $|A| = {}$, $downscale factor = {}$, $pixel intensity = {}$]'.format(self.epsilon, len(self.actions), self.s_e_p.resize_factor, self.s_e_p.pixel_intensity)
